c29e6ffaa8431f1f29cff904bbf8b365b7803dbf
Akshay Patil <akshay@mclabs.co.in>
2026-04-28 13:54:27 +0530

    Update dummy-server-main.go

--- examples/rx_mbms_server/dummy-server-main.go
+++ examples/rx_mbms_server/dummy-server-main.go
@@ -22,10 +22,10 @@ func main() {
 	flag.Parse()
 
 	settings := &sm.Settings{
-		OriginHost: datatype.DiameterIdentity(*host),
-		OriginRealm: datatype.DiameterIdentity(*realm),
-		VendorID: diam.TGPP_VENDOR_ID,
-		ProductName: "dummy-rx-mb2c-server",
+		OriginHost:       datatype.DiameterIdentity(*host),
+		OriginRealm:      datatype.DiameterIdentity(*realm),
+		VendorID:         diam.TGPP_VENDOR_ID,
+		ProductName:      "dummy-rx-mb2c-server",
 		FirmwareRevision: 1,
 	}
 
@@ -38,16 +38,24 @@ func main() {
 
 	log.Printf("dummy Diameter server listening on %s origin=%s realm=%s", *addr, *host, *realm)
 	log.Printf("supported handlers: Rx AAR/AAA app=%d, MB2-C GAR/GAA app=%d", diam.RX_APP_ID, diam.MB2C_APP_ID)
-	if err := diam.ListenAndServe(*addr, mux, nil); err != nil { log.Fatal(err) }
+	if err := diam.ListenAndServe(*addr, mux, nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
-func printErrors(ec <-chan *diam.ErrorReport) { for err := range ec { log.Printf("diameter error: %v", err) } }
+func printErrors(ec <-chan *diam.ErrorReport) {
+	for err := range ec {
+		log.Printf("diameter error: %v", err)
+	}
+}
 
 func handleRxAAR(settings *sm.Settings, verbose bool) diam.HandlerFunc {
 	return func(c diam.Conn, m *diam.Message) {
 		log.Printf("RX: received AAR from %s session=%s app=%d", c.RemoteAddr(), avpString(m, avp.SessionID), m.Header.ApplicationID)
-		logInterestingAVPs("RX", m, []interface{}{avp.AFAppId, avp.MediaCompDescp, avp.RxRequestType, avp.SubscriptionID})
-		if verbose { log.Printf("RX: incoming AAR\n%s", m) }
+		logInterestingAVPs("RX", m, []interface{}{avp.AFApplicationIdentifier, avp.MediaComponentDescription, avp.RxRequestType, avp.SubscriptionID})
+		if verbose {
+			log.Printf("RX: incoming AAR\n%s", m)
+		}
 		a := m.Answer(diam.Success)
 		copyAVP(a, m, avp.SessionID, 0)
 		addServerOrigin(a, settings)
@@ -61,7 +69,9 @@ func handleMB2CGAR(settings *sm.Settings, verbose bool) diam.HandlerFunc {
 	return func(c diam.Conn, m *diam.Message) {
 		log.Printf("MB2-C: received GAR from %s session=%s app=%d", c.RemoteAddr(), avpString(m, avp.SessionID), m.Header.ApplicationID)
 		logInterestingAVPs("MB2-C", m, []interface{}{avp.SupportedFeatures, avp.TMGIAllocationRequest, avp.TMGIDeallocationRequest, avp.TMGINumber, avp.TMGI})
-		if verbose { log.Printf("MB2-C: incoming GAR\n%s", m) }
+		if verbose {
+			log.Printf("MB2-C: incoming GAR\n%s", m)
+		}
 		a := m.Answer(diam.Success)
 		copyAVP(a, m, avp.SessionID, 0)
 		copyAVP(a, m, avp.AuthApplicationID, 0)
@@ -88,23 +98,54 @@ func addMB2CActionResponse(a, req *diam.Message) {
 }
 
 func firstTMGI(m *diam.Message) datatype.OctetString {
-	if a, err := m.FindAVP(avp.TMGI, diam.TGPP_VENDOR_ID); err == nil { if v, ok := a.Data.(datatype.OctetString); ok && v != "" { return v } }
+	if a, err := m.FindAVP(avp.TMGI, diam.TGPP_VENDOR_ID); err == nil {
+		if v, ok := a.Data.(datatype.OctetString); ok && v != "" {
+			return v
+		}
+	}
 	return defaultTMGI
 }
 
-func addServerOrigin(m *diam.Message, settings *sm.Settings) { m.NewAVP(avp.OriginHost, avp.Mbit, 0, settings.OriginHost); m.NewAVP(avp.OriginRealm, avp.Mbit, 0, settings.OriginRealm) }
-func copyAVP(dst, src *diam.Message, code interface{}, vendorID uint32) { if a, err := src.FindAVP(code, vendorID); err == nil && a != nil { dst.AddAVP(a) } }
-func avpString(m *diam.Message, code interface{}) string { if a, err := m.FindAVP(code, 0); err == nil && a != nil && a.Data != nil { return a.Data.String() }; return "" }
+func addServerOrigin(m *diam.Message, settings *sm.Settings) {
+	m.NewAVP(avp.OriginHost, avp.Mbit, 0, settings.OriginHost)
+	m.NewAVP(avp.OriginRealm, avp.Mbit, 0, settings.OriginRealm)
+}
+func copyAVP(dst, src *diam.Message, code interface{}, vendorID uint32) {
+	if a, err := src.FindAVP(code, vendorID); err == nil && a != nil {
+		dst.AddAVP(a)
+	}
+}
+func avpString(m *diam.Message, code interface{}) string {
+	if a, err := m.FindAVP(code, 0); err == nil && a != nil && a.Data != nil {
+		return a.Data.String()
+	}
+	return ""
+}
 
 func logInterestingAVPs(prefix string, m *diam.Message, codes []interface{}) {
 	var parts []string
 	for _, code := range codes {
 		avps, err := m.FindAVPs(code, diam.TGPP_VENDOR_ID)
-		if err != nil || len(avps) == 0 { avps, err = m.FindAVPs(code, 0) }
-		if err == nil && len(avps) > 0 { parts = append(parts, fmt.Sprint(code)+"="+avps[0].Data.String()) }
+		if err != nil || len(avps) == 0 {
+			avps, err = m.FindAVPs(code, 0)
+		}
+		if err == nil && len(avps) > 0 {
+			parts = append(parts, fmt.Sprint(code)+"="+avps[0].Data.String())
+		}
 	}
-	if len(parts) > 0 { log.Printf("%s: key AVPs: %s", prefix, strings.Join(parts, ", ")) }
+	if len(parts) > 0 {
+		log.Printf("%s: key AVPs: %s", prefix, strings.Join(parts, ", "))
+	}
 }
 
-func writeAnswer(prefix string, c diam.Conn, a *diam.Message, verbose bool) { if verbose { log.Printf("%s: outgoing answer\n%s", prefix, a) }; if _, err := a.WriteTo(c); err != nil { log.Printf("%s: failed to write answer to %s: %v", prefix, c.RemoteAddr(), err) } }
-func handleAll(c diam.Conn, m *diam.Message) { log.Printf("received unhandled Diameter message from %s cmd=%d app=%d flags=0x%x\n%s", c.RemoteAddr(), m.Header.CommandCode, m.Header.ApplicationID, m.Header.CommandFlags, m) }
+func writeAnswer(prefix string, c diam.Conn, a *diam.Message, verbose bool) {
+	if verbose {
+		log.Printf("%s: outgoing answer\n%s", prefix, a)
+	}
+	if _, err := a.WriteTo(c); err != nil {
+		log.Printf("%s: failed to write answer to %s: %v", prefix, c.RemoteAddr(), err)
+	}
+}
+func handleAll(c diam.Conn, m *diam.Message) {
+	log.Printf("received unhandled Diameter message from %s cmd=%d app=%d flags=0x%x\n%s", c.RemoteAddr(), m.Header.CommandCode, m.Header.ApplicationID, m.Header.CommandFlags, m)
+}
